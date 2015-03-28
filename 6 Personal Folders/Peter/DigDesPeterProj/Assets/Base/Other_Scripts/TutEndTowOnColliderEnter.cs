9d35fbdb080bf232e353c4210b689501f1bf4d82
BigHandInSky <bighandinsky@gmail.com>
2015-03-28 21:40:43 +0000

    Completed Tutorial, Tutorial Design

---
+++ 6 Personal Folders/Peter/DigDesPeterProj/Assets/Base/Other_Scripts/TutEndTowOnColliderEnter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutEndTowOnColliderEnter : MonoBehaviour {
+
+    [SerializeField]
+    private string sNextTutScene;
+    [SerializeField]
+    private string sPlayerTag;
+
+    void OnColliderEnter(Collision other)
+    {
+        Debug.Log("triggered");
+
+        if (other.gameObject.tag == sPlayerTag)
+        {
+            Application.LoadLevel(sNextTutScene);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triggered");
+
+        if (other.gameObject.tag == sPlayerTag)
+        {
+            Application.LoadLevel(sNextTutScene);
+        }
+    }
+}
