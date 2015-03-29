278a2408bed0abed765bd10d00efdc892ba5d4c3
PenguinX45654 <penguinalex1@msn.com>
2015-03-29 03:59:28 +0100

    Basic Level Viewer
    Basic Level Viewer that can open level xmls with an OpenFileDialog and
    has a simple flying camera to be able to view a level from multiple
    angles.
    
    Currently only in editor, has not been tested as a standalone but should
    be stable.

---
+++ 6 Personal Folders/Alex/Assets/Scripts/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// Controls the movement and rotation of the camera
+
+public class CameraController : MonoBehaviour 
+{
+    // Original Transform for the camera to reset to
+    public Transform tOriginalTransform;
+
+    // Move Speed
+    public float fSpeed;
+    // Turn Speed
+    public float fMouseSensitivity;
+
+	// Initialization
+	void Start () 
+    {
+	}
+	
+	// Update 
+	void Update () 
+    {
+        // Movement Axes
+        float xAxis = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float yAxis = Input.GetAxis("Altitude") * Time.deltaTime;
+        float zAxis = Input.GetAxis("Vertical") * Time.deltaTime;
+        // Rotation Axes
+        float mouseX = Input.GetAxis("Mouse Y") * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse X") * Time.deltaTime;
+
+        // Move the camera with the input from the Movement Axes
+        transform.Translate((transform.right * xAxis * fSpeed) +
+            (Vector3.up * yAxis * fSpeed) +
+            (transform.forward * zAxis * fSpeed), Space.World);
+
+        // If the Drag Button is held and the mouse is being dragged then rotate the camera.
+        if (Input.GetButton("Drag"))
+        {
+            // Horizontal Rotation - Yaw
+            Quaternion xRot = Quaternion.Euler(Vector3.right * mouseX * fMouseSensitivity);
+            // Vertical Rotation - Pitch
+            Quaternion yRot = Quaternion.Euler(Vector3.up * mouseY * fMouseSensitivity);
+
+            // Calculate new rotation
+            Quaternion rotation = transform.rotation * xRot * yRot;
+            // Reset Z Rotation to 0 - Roll
+            rotation = Quaternion.Euler(rotation.eulerAngles.x, rotation.eulerAngles.y, 0);
+
+            // Apply new rotation
+            transform.rotation = rotation;
+        }
+	}
+
+    // Resets the transform of the camera to the original transform
+    public void ResetPosition()
+    {
+        transform.position = tOriginalTransform.position;
+        transform.rotation = tOriginalTransform.rotation;
+    }
+}
