120b62a6b54d951ae47165594eeb5e1839f46094
BigHandInSky <bighandinsky@gmail.com>
2015-04-16 22:56:02 +0100

    BFXR sound files and Artwork
    - Added BFXR sound files from testing for merge project
    - Tested Credit design idea
    - Tested Main UI TitleCard designs
    - Created cursor PeNuG
    - Added to Game Settings to handle settings
    - Notes from 16042015 merge meet
    - Unitypackage of Base pre-merge

--- 6 Personal Folders/Peter/DigDesPeterProj/Assets/Base/Menu_Scripts/GameSettings.cs
+++ 6 Personal Folders/Peter/DigDesPeterProj/Assets/Base/Menu_Scripts/GameSettings.cs
@@ -16,5 +16,39 @@ public class GameSettings : MonoBehaviour {
     void Awake()
     {
         m_DataInstance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private bool bMusicOn = true;
+    public bool Music { get { return bMusicOn; } set { bMusicOn = value; } }
+    private bool bEffectsOn = true;
+    public bool Effects { get { return bEffectsOn; } set { bEffectsOn = value; } }
+
+    private float fVolume = 10;
+    public float Volume { get { return fVolume; } }
+    private float fFOV = 10f;
+    public float FOV { get { return fFOV; } }
+
+    private int iResWidth = 800;
+    public int RWidth { get { return iResWidth; } }
+    private int iResHeight = 600;
+    public int RHeight { get { return iResHeight; } }
+
+
+    public void SetResolution(int _width, int _height)
+    {
+        iResWidth = _width;
+        iResHeight = _height;
+        Screen.SetResolution(_width, _height, true);
+    }
+    public void SetVolume(float _Val)
+    {
+        fVolume = _Val;
+        AudioListener.volume = _Val;
+    }
+    public void SetFOV(float _Val)
+    {
+        fFOV = _Val;
+        //Camera.main.fieldOfView = _Val;
     }
 }
