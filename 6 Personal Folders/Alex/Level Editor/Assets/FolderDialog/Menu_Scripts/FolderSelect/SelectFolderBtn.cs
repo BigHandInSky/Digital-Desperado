f1b2dae793e1cf09b0651ca3012c121801464e30
BigHandInSky <bighandinsky@gmail.com>
2015-04-20 19:54:12 +0100

    Made Standardised UI for LevelEditor
    - Imported Folder UI and commented errors
    - Added txt file documenting how new Ui should work
    - Added Popup canvas
    - Added Main canvas
    - Set both to scale by width

---
+++ 6 Personal Folders/Alex/Level Editor/Assets/FolderDialog/Menu_Scripts/FolderSelect/SelectFolderBtn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SelectFolderBtn : MonoBehaviour {
+
+    [SerializeField] private FolderUIControl FolderParent;
+    [SerializeField] private FolderListManager Manager;
+
+    public void Setup()
+    {
+        StartCoroutine(Check());
+    }
+
+    IEnumerator Check()
+    {
+        while (true)
+        {
+            gameObject.GetComponent<Button>().interactable = Manager.SuitableDirectory;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void Select()
+    {
+        //MenuLoadLevelsFromXML.Instance.GetFolder(Manager.DirectoryUrl);
+        FolderParent.CloseFolderUI();
+    }
+}
