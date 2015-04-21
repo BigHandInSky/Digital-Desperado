838f37020bdc9d60fec2413d454460b5eb895c67
PenguinX45654 <penguinalex1@msn.com>
2015-04-21 15:50:40 +0100

    Level Editor UI Updated
    Updated the Level Editor UI to Peter's design and have began to
    implement object selection into the editor. However, selection does not
    currently work

---
+++ 6 Personal Folders/Alex/Level Editor/Assets/Scripts/SelectObject.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectObject : MonoBehaviour 
+{
+    Camera levelCamera;
+
+    [SerializeField]
+    LayerMask collisionMask;
+
+    GameObject goSelectedObject;
+
+    [SerializeField]
+    Material mSelectMaterial;
+    Material mObjectOriginalMaterial;
+
+    [SerializeField]
+    GameObject goEditObjectPanel;
+
+    public GameObject ObjectSelected
+    {
+        get { return goSelectedObject; }
+    }
+
+    void Awake()
+    {
+        levelCamera = Camera.main;
+        goSelectedObject = null;
+        mObjectOriginalMaterial = null;
+
+        //StartCoroutine("UpdateClick");
+    }
+
+    //IEnumerator UpdateClick()
+    void Update()
+    {
+        //print("Start Running");
+
+        //while (true)
+        //{
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("Click");
+
+                RaycastHit hit;
+
+                Physics.Raycast(levelCamera.transform.position, levelCamera.ScreenToWorldPoint(Input.mousePosition),
+                    out hit, 100000, collisionMask);
+
+                if (hit.collider != null)
+                {
+                    DeselectObject();
+                    SelectNewObject(hit.collider.gameObject);
+                }
+                else
+                {
+                    DeselectObject();
+                }
+            }
+
+            //yield return new WaitForEndOfFrame();
+        //}
+    }
+
+    private void SelectNewObject(GameObject clickedObject)
+    {
+        print("Select");
+        print(clickedObject);
+
+        Renderer objectRenderer = goSelectedObject.GetComponent<MeshRenderer>();
+
+        goSelectedObject = clickedObject;
+        mObjectOriginalMaterial = objectRenderer.material;
+        objectRenderer.material = mSelectMaterial;
+
+        goEditObjectPanel.SetActive(true);
+    }
+
+    private void DeselectObject()
+    {
+        print("Deselect Object");
+
+        if (goSelectedObject == null || mObjectOriginalMaterial == null)
+        {
+            return;
+        }
+
+        goSelectedObject.GetComponent<Renderer>().material = mObjectOriginalMaterial;
+
+        goSelectedObject = null;
+        mObjectOriginalMaterial = null;
+
+        goEditObjectPanel.SetActive(false);
+    }
+}
