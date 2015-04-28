618ef971dc2c34e44b42b4a4a85f3463b2b832e1
BigHandInSky <bighandinsky@gmail.com>
2015-04-28 15:53:03 +0100

    Build v09

---
+++ 5 Merge Project/DigitalDesperadoMerge/Assets/Base/Other_Scripts/GeneralControlKeys.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneralControlKeys : MonoBehaviour 
+{
+
+	void Update () 
+    {
+	    if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else if (Input.GetKey(GameSettings.Instance.Menu) && !Application.loadedLevelName.Contains("Main"))
+        {
+            Application.LoadLevel("Main");
+        }
+        else if (Input.GetKey(GameSettings.Instance.Rset) && Application.loadedLevelName.Contains("Game"))
+        {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<RestartLevel>().DoRestart();
+        }
+	}
+}
